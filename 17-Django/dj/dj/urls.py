5eecb207fce459fe3f1579a1bde835dc67167fe0
flyingtothe <41361359+flyingtothe@users.noreply.github.com>
2018-09-17 21:03:35 +0800

    2018/9/17 modify

--- 17-Django/dj/dj/urls.py
+++ 17-Django/dj/dj/urls.py
@@ -1,4 +1,4 @@
-"""dj URL Configuration
+"""tulingxueyuan URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -14,17 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from teacher import views as tv
+from django.urls import path, re_path, include
+from teacher import views, teacher_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # 视图函数只有名称，无括号、参数
 
-    # 尖括号表示以后面内容开头的表达式
+    # 视图函数只有名称，无括号、参数
+    path('normalmap/', views.do_normalmap),
+
+    # 使用尖括号 <> 从 url 中捕获值
     # 圆括号表示的是一个参数，里面的内容作为参数传递个被调用的函数
-    # 参数名称以问号加大写P开头，尖括号内是参数的名字
-    # 尖括号后表示正则，[0-9]表示内容仅能是由0-9的数字构成
+    # 参数名称以 ?P 开头，尖括号内是参数的名字
+    # 尖括号后表示正则
     # 大括号表示出现的次数
-    path('withparam/<int:year>/<int:month>/', tv.withparam),
+    re_path('withparam/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/', views.withparam),
+
+    # 约定，凡是有 teacher 模块处理的视图的 url 都已 teacher 开头
+    path('teacher/', include(teacher_url)),
+
+    re_path(r'^book/(?:page-(?P<pn>\d+)/)$', views.do_param2),
+
+    # re_path('yourname/', views.extremParam, {'name': 'bar'}),
+
+    path('yourname/', views.revParse, name='askname'),
 ]
--- 17-Django/dj/teacher/views.py
+++ 17-Django/dj/teacher/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse
 
 # Create your views here.
 
@@ -11,3 +12,15 @@ def do_normalmap(request):
 
 def withparam(request, year, month):
     return HttpResponse('this is with param {0}, {1}'.format(year, month))
+
+def do_app(r):
+    return HttpResponse('这是个子路由')
+
+def do_param2(r, pn):
+    return HttpResponse('page number is {0}'.format(pn))
+
+def extremParam(r,name):
+    return HttpResponse('my name is {0}'.format(name))
+
+def revParse(r):
+    return HttpResponse('your requesred url is {0}'.format(reverse(viewname='askname')))
