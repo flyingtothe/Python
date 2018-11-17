c5fce65ff1b428d4a0fa67f76cf8729415a61422
flyingtothe <41361359+flyingtothe@users.noreply.github.com>
2018-11-17 15:55:45 +0800

    18/11/17 modify

--- 17-Django/myAdmin/addmin/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from addmin.models import Sudent, ClassRoom, Teacher
-
-# Register your models here.
-
-admin.site.register(Sudent)
-admin.site.register(ClassRoom)
-admin.site.register(Teacher)
